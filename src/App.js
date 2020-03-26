657b48fa2c9e1377cc71d99d7ff609f7f19d48f5
siva.sundar <siva.sundar@philips.com>
2020-03-26 19:48:01 +0530

    my portfolio coming soon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './images/Website-Design-Background.jpg'
+// import logo from './images/images.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="back-image">
+      <p>My Portfolio coming soon!!</p>
     </div>
   );
 }
